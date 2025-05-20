ebdef834d1da6ce96b21e9d0e23ee6c5fbc26581
randyduong08 <randyduong08@gmail.com>
2025-05-19 20:32:19 -0400

    update heatmaps page.tsx

--- client-quantbox/src/app/black-scholes/greeks-visualization/page.tsx
+++ client-quantbox/src/app/black-scholes/greeks-visualization/page.tsx
@@ -1,21 +1,22 @@
 import Head from "next/head";
-import {JSX} from "react";
+import { JSX } from "react";
 import GreeksCharts from "@/components/greeks-charts/greeks-charts";
 
 export default function GreeksVisualizationPage(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>QuantBox - Option Greeks Visualization</title>
+        <meta
+          name={"description"}
+          content={"Interactive visualization of option Greeks"}
+        />
+        <link rel={"icon"} href={"/favicon.ico"} />
+      </Head>
 
-
-    return(
-        <>
-            <Head>
-                <title>QuantBox - Option Greeks Visualization</title>
-                <meta name={"description"} content={"Interactive visualization of option Greeks"}/>
-                <link rel={"icon"} href={"/favicon.ico"}/>
-            </Head>
-
-            <main>
-                <GreeksCharts/>
-            </main>
-        </>
-    )
-};
+      <main>
+        <GreeksCharts />
+      </main>
+    </>
+  );
+}
--- client-quantbox/src/app/black-scholes/heatmaps-visualization/page.tsx
+++ client-quantbox/src/app/black-scholes/heatmaps-visualization/page.tsx
@@ -1,10 +1,22 @@
-import {JSX} from "react";
+import Head from "next/head";
+import { JSX } from "react";
 import OptionsHeatmap from "@/components/options-heatmap/options-heatmap";
 
 export default function HeatmapsVisualizationPage(): JSX.Element {
-    return(
-        <>
-            <OptionsHeatmap/>
-        </>
-    )
-};
+  return (
+    <>
+      <Head>
+        <title>QuantBox - Options price Heatmap</title>
+        <meta
+          name={"description"}
+          content={"Heatmap visualization on how option prices fluctuate"}
+        />
+        <link rel={"icon"} href={"/favicon.ico"} />
+      </Head>
+
+      <main>
+        <OptionsHeatmap />
+      </main>
+    </>
+  );
+}
