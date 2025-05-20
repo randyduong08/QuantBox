94df507eaacef52d0b458b400210ad9bfce0f229
randyduong08 <randyduong08@gmail.com>
2025-05-20 18:54:39 -0400

    init backend
    init new cargo project, which will be the backend (axum)

---
+++ client-quantbox/src/app/backtest/page.tsx
@@ -0,0 +1,9 @@
+import { JSX } from "react";
+
+export default function BacktestPage(): JSX.Element {
+  return (
+    <>
+      <p>Testing backtest</p>
+    </>
+  );
+}
---
+++ server-quantbox/target/debug/deps/server_quantbox-ef5e3fed10b184c6.d
@@ -0,0 +1,9 @@
+C:\source\repos\QuantBox\server-quantbox\target\debug\deps\libserver_quantbox-ef5e3fed10b184c6.rmeta: src\main.rs Cargo.toml
+
+C:\source\repos\QuantBox\server-quantbox\target\debug\deps\server_quantbox-ef5e3fed10b184c6.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-W__CLIPPY_HACKERY__clippy::pedantic__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
