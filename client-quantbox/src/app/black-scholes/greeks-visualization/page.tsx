02a26f5c8c6cc0d968588d52e8a33dd81feeb14c
randyduong08 <randyduong08@gmail.com>
2025-05-20 09:52:29 -0400

    black-scholes layout.tsx styling changes
    edited layout.tsx to make so black-scholes pages are at minimum 100vh

--- client-quantbox/src/app/black-scholes/greeks-visualization/page.tsx
+++ client-quantbox/src/app/black-scholes/greeks-visualization/page.tsx
@@ -5,18 +5,18 @@ import GreeksCharts from "@/components/greeks-charts/greeks-charts";
 export default function GreeksVisualizationPage(): JSX.Element {
   return (
     <>
-      <Head>
-        <title>QuantBox - Option Greeks Visualization</title>
-        <meta
-          name={"description"}
-          content={"Interactive visualization of option Greeks"}
-        />
-        <link rel={"icon"} href={"/favicon.ico"} />
-      </Head>
+      <div className={"bg-gray-900 w-full"}>
+        <Head>
+          <title>QuantBox - Option Greeks Visualization</title>
+          <meta
+            name={"description"}
+            content={"Interactive visualization of option Greeks"}
+          />
+          <link rel={"icon"} href={"/favicon.ico"} />
+        </Head>
 
-      <main>
         <GreeksCharts />
-      </main>
+      </div>
     </>
   );
 }
--- client-quantbox/src/app/black-scholes/heatmaps-visualization/page.tsx
+++ client-quantbox/src/app/black-scholes/heatmaps-visualization/page.tsx
@@ -5,18 +5,18 @@ import OptionsHeatmap from "@/components/options-heatmap/options-heatmap";
 export default function HeatmapsVisualizationPage(): JSX.Element {
   return (
     <>
-      <Head>
-        <title>QuantBox - Options price Heatmap</title>
-        <meta
-          name={"description"}
-          content={"Heatmap visualization on how option prices fluctuate"}
-        />
-        <link rel={"icon"} href={"/favicon.ico"} />
-      </Head>
+      <div className={"bg-gray-900 w-full"}>
+        <Head>
+          <title>QuantBox - Options price Heatmap</title>
+          <meta
+            name={"description"}
+            content={"Heatmap visualization on how option prices fluctuate"}
+          />
+          <link rel={"icon"} href={"/favicon.ico"} />
+        </Head>
 
-      <main>
         <OptionsHeatmap />
-      </main>
+      </div>
     </>
   );
 }
